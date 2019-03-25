f8792388299c0487049c281c25571fda954dd00c
Gabriel Dalporto <dalporto@yahoo.com>
2019-03-25 12:01:59 -0700

    working client with core functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -33,14 +33,138 @@ export const authError = error => ({
     error
 });
 
-export const LOG_TREATMENT = 'LOG_TREATMENT';
-export const logTreatment = (treatmentObject) => ({
-    type: LOG_TREATMENT,
-    date: treatmentObject.date,
-    treatment: treatmentObject.treatment,
-    status: treatmentObject.status,
-    currentUser: treatmentObject.currentUser
+export const LOG_TREATMENT_STATE = 'LOG_TREATMENT_STATE';
+export const logTreatmentState = (user) => ({
+    type: LOG_TREATMENT_STATE,
+    user
 });
+
+
+
+export const logTreatment = (treatmentObject) => (dispatch)=> {
+
+    let treatmentProfile = {
+        date: treatmentObject.date,
+        treatment: treatmentObject.treatment,
+        status: treatmentObject.status,
+        currentUser: treatmentObject.currentUser
+    }
+    console.log("INSIDE LOGTREATMENT. TREATMENTPROFILE IS", treatmentProfile);
+
+        let user = treatmentProfile.currentUser;
+        console.log("INSIDE REDUCER USER IS". user);
+        let newLog={};
+        let dateList = {};
+
+        // interate over each log date to create a master date list.
+        user.log.forEach(log => {
+            dateList = {...dateList, ...log}
+        })
+        let flatDateList = Object.keys(dateList);
+        let dateKey = [treatmentProfile.date]
+        flatDateList.forEach(date=>{
+            if(date!==dateKey[0]){
+                dateKey = [...dateKey, date]
+            }
+        })
+        dateKey.sort();
+
+        // iterate over each date log to process new set of logs
+        dateKey.forEach((date,index)=>{
+
+            // if dateKey doesn't match action date, then copy the old logs into the new logs
+            const logitem = user.log[index];
+            if(treatmentProfile.date !== date){
+                newLog={...newLog, ...logitem}
+                console.log("INSIDE FOREACH, DATE NOT MATCHED, NEWLOG IS", newLog)
+            }
+
+            // if datekey does match action date, 
+            else if(treatmentProfile.date === date) {
+                let actionLog={};
+
+                //if no existing logs for this date, then append new action and status 
+                if(!logitem){
+                    actionLog={[treatmentProfile.date]:{[treatmentProfile.treatment]: treatmentProfile.status}};
+                    newLog={...newLog, ...actionLog}
+                    console.log("INSIDE FOREACH, DATE MATCHED, NO EXISTING, NEWLOG IS", newLog);
+                }
+
+                // if there are existing treatments, then iterate over each entry, appending new entry. 
+                else {
+                    const newActionEntry = {[treatmentProfile.treatment]: treatmentProfile.status};
+                    let logList={};
+                    let logEntry={}
+                    let logDate=Object.keys(logitem);
+                    let logKeys = Object.keys(logitem[logDate]);
+                    let treatment=treatmentProfile.treatment
+                    let totalLogKeys = [treatment];
+                    logKeys.forEach(key=>{
+                        if(key!==totalLogKeys[0]){
+                            totalLogKeys = [...totalLogKeys, key]
+                        }
+                    })
+                    totalLogKeys.forEach((key)=>{
+                        const logItem = user.log[index];
+                        const dayLogs=logItem[logDate];
+                        const keyStatus=dayLogs[key];
+                        logEntry = {[key]:keyStatus};
+                        logList={...logList, ...logEntry, ...newActionEntry}
+
+                    })
+                    actionLog={[treatmentProfile.date]:logList}
+                    newLog={...newLog, ...actionLog};
+                    console.log("INSIDE FOREACH, DATE MATCHED, EXISTING, NEWLOG IS", newLog);
+                }
+            }
+        })
+        console.log("NEWLOG AFTER DATEKEY ITERATION", newLog);
+        let newLogArray =Object.keys(newLog).map(key => {
+            return { [key]:newLog[key] }
+        })
+
+        user = {
+            ...user, 
+            log: newLogArray
+        }
+        console.log("INSIDE LOGTREATMENT. USER IS", user);
+
+        console.log("DISPATCHING NEWAUTHTOKENPAYLOAD");
+        // update auth token with updated log payload
+        dispatch(newAuthTokenPayload(user));
+
+        console.log("DISPATCHING LOGTREATMENTSTATE")
+        // update state with new log
+        dispatch(logTreatmentState(user));
+
+
+        console.log("DISPATCHING DB CALL TO UPDATELOG")
+        // update user log in database
+        fetch(`${API_BASE_URL}/user/updatelog`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => normalizeResponseErrors(res))
+            .then(res => res.json())
+            .then(jsonRes => console.log("AFTER DB CALL, RESPONSE OBJECT IS",jsonRes))
+            .catch(err => {
+                const {reason, message, location} = err;
+                if (reason === 'ValidationError') {
+                    // Convert ValidationErrors into SubmissionErrors for Redux Form
+                    return Promise.reject(
+                        new SubmissionError({
+                            [location]: message
+                        })
+                    );
+                }
+            });
+        
+};
+
+
 
 
 
@@ -87,6 +211,45 @@ export const login = (username, password) => dispatch => {
                 );
             })
     );
+};
+
+export const NEW_AUTH_TOKEN_PAYLOAD = 'NEW_AUTH_TOKEN_PAYLOAD';
+export const newAuthTokenPayload = (payload) => (dispatch, getState) => {
+    dispatch(authRequest());
+    const authToken = getState().auth.authToken;
+//    const payload = getState().auth.currentUser;
+    console.log("PAYLOAD", JSON.stringify(payload));
+    return fetch(`${API_BASE_URL}/auth/refresh/newpayload`, {
+        method: 'POST',
+        headers: {
+            // Provide our existing token as credentials to get a new one
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`
+        },
+        body: JSON.stringify({
+            id:payload.id,
+            username: payload.username,
+            condition: payload.condition,
+            treatments: payload.treatments,
+            log: payload.log,
+            error: null,
+            loading: false
+        })
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(({authToken}) => {
+            console.log("NEW AUTH TOKEN IS", authToken)
+            storeAuthInfo(authToken, dispatch)
+        })
+        .catch(err => {
+            // We couldn't get a refresh token because our current credentials
+            // are invalid or expired, or something else went wrong, so clear
+            // them and sign us out
+            dispatch(authError(err));
+            dispatch(clearAuth());
+            clearAuthToken(authToken);
+        });
 };
 
 export const refreshAuthToken = () => (dispatch, getState) => {
