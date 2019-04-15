c64b5aa4db37c75e92e50f600846b0c87da0db98
Gabriel Dalporto <dalporto@yahoo.com>
2019-04-15 16:19:38 -0700

    reference to heroku server

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'; 
+    process.env.REACT_APP_API_BASE_URL || 'https://obscure-eyrie-82877.herokuapp.com' || 'http://localhost:8080'; 
+
